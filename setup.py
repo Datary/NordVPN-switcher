4ce321f0404725784b4bf5f9009a9ed46cddf9c3
Fernando Diaz Laclaustra <peke_buda@hotmail.com>
2021-09-21 11:40:07 +0200

    Sustituye empaquetamiento basado en setup.py por el basado en setup.cfg.

--- setup.py
+++ setup.py
@@ -1,44 +1,10 @@
-from pathlib import Path
-from setuptools import setup
+#!/usr/bin/env python
 
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
 
-DIRNAME = Path(__file__).parent
-
-VERSION = '0.0.1'
-PACKAGE_NAME = 'datary-nordvpn-switcher'
-AUTHOR = 'Datary'
-AUTHOR_EMAIL = 'engineering@datary.io'
-URL = 'https://github.com/datary/nordvpn_switcher'
-LICENSE = 'Apache License 2.0'
-DESCRIPTION = 'Automatic endpoint switcher for NordVPN'
-LONG_DESCRIPTION = (DIRNAME / 'README.md').read_text()
-LONG_DESC_TYPE = 'text/markdown'
-INSTALL_REQUIRES = [
-    'psutil',
-    'bs4',
-    'requests',
-    'lxml',
-    'pathlib',
-    'random_user_agent'
-]
-
-setup(
-    name=PACKAGE_NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type=LONG_DESC_TYPE,
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    license=LICENSE,
-    url=URL,
-    install_requires=INSTALL_REQUIRES,
-    package_dir={
-        'datary_nordvpn_switcher': 'src'
-    },
-    packages=[
-        'datary_nordvpn_switcher',
-        'datary_nordvpn_switcher.NordVPN_options'
-    ],
-    include_package_data=True
-)
+setup(setup_cfg=True)
